14904f6b6716599e1f2509a46dd4d5ab6a9a3515
Evan Dorn <evan@lrdesign.com>
2014-02-21 15:42:40 -0800

    Correctly check for specified environment and presence of adapter. Environment defaults to development.

--- spec/bibliotech/config_loader_spec.rb
+++ spec/bibliotech/config_loader_spec.rb
@@ -1,28 +1,97 @@
 require 'spec_helper'
 module BiblioTech
+  class ::Hash
+    def deep_merge(second)
+      merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : v2 }
+      self.merge(second, &merger)
+    end
+  end
+
   describe ConfigLoader do
     describe 'initialization' do
+      before do
+        File.stub(:open).and_return(file)
+        YAML.stub(:load).and_return(hash)
+      end
 
       context 'with a filename provided' do
         let :path do '/some/path/database.yml' end
-        let :file do mock(File) end
-        let :hash do { :some => :values } end
-
-        it "should load that file and parse with yaml" do
-          File.should_receive(:open).with(path).and_return(file)
-          YAML.should_receive(:load).with(file).and_return(hash)
-          ConfigLoader.new(path)
+        let :file do double(File) end
+        let :valid_hash do
+          { :development =>
+            { :username => 'root',
+              :database  => 'dev_db',
+              :adapter   => 'mysql',
+            },
+            :production =>
+            { :username => 'root',
+              :database  => 'dev_db',
+              :adapter   => 'mysql2',
+            }
+          }
         end
 
-        it "should produce an error if the file does not contain database configs" do
-          pending
+        context "if the file contains database configs" do
+          let :hash do valid_hash end
+          it "should load that file and parse with yaml" do
+            File.should_receive(:open).with(path).and_return(file)
+            YAML.should_receive(:load).with(file).and_return(hash)
+            ConfigLoader.new(path)
+          end
+
+          context "with default(development) environment" do
+            it "should make the development hash available at config" do
+              loader = ConfigLoader.new(path)
+              loader.config.should == valid_hash[:development]
+            end
+          end
+
+          context "with specified environment" do
+            it "should make the symbol-specified hash available at config" do
+              loader = ConfigLoader.new(path, :production)
+              loader.config.should == valid_hash[:production]
+            end
+            it "should make the string-specified hash available at config" do
+              loader = ConfigLoader.new(path, 'production')
+              loader.config.should == valid_hash[:production]
+            end
+          end
+
         end
 
-        it "should make the equivalent hash available at config" do
-          File.stub(:open).and_return(file)
-          YAML.stub(:load).and_return(hash)
-          loader = ConfigLoader.new(path)
-          loader.config.should == hash
+        context "when the file contains bad configs" do
+          context "with no environments" do
+            let :hash do { :some => 'values'} end
+            it "should raise an error" do
+              expect do
+                ConfigLoader.new(path)
+              end.to raise_error
+            end
+          end
+
+          context "with no matching environment" do
+            let :hash do  valid_hash.reject(){|k,v| k == :development } end
+            it "should raise an error" do
+              expect do
+                ConfigLoader.new(path)
+              end.to raise_error
+            end
+          end
+
+          context "with no adapter" do
+            let :hash do
+              valid_hash.deep_merge({
+                :development => { :adapter => nil },
+              })
+            end
+
+            it "should raise an error" do
+              expect do
+                ConfigLoader.new(path)
+              end.to raise_error
+            end
+          end
+
         end
       end
 
